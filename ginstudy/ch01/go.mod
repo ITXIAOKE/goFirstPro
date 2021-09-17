98403c640373d0473267b455b8b25157019cf99f
cuixiaoke <976249817@qq.com>
2021-09-17 22:47:01 +0800

    gin框架初步认识

--- ginstudy/ch01/go.mod
+++ ginstudy/ch01/go.mod
@@ -1,3 +1,5 @@
 module ginstudy.com/ch031
 
 go 1.16
+
+require github.com/gin-gonic/gin v1.7.3
--- ginstudy/ch01/main.go
+++ ginstudy/ch01/main.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello go")
+	fmt.Println("hello 11111111111")
+	r:=gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200,gin.H{
+			"message":"pong",
+		})
+	})
+	r.Run()
+	fmt.Println("hello 2222222222")
 }
 
 
